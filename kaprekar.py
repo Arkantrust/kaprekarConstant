05b73aaaaa23e6bec74483bfac110e9342af8863
Arkantrust <davidulce26@gmail.com>
2023-06-14 04:42:23 -0500

    Optimized kaprekar()

--- kaprekar.py
+++ kaprekar.py
@@ -1,34 +1,25 @@
-"""
-For any 4-digit number with at least 2 different digits. If arranged in descending and ascending order and subtracted,
-eventually we will get Kaprekar's constant = 6174.
-"""
-
-KAPREKAR_CONSTANT = 6174
-
-
-def kaprekar(candidate: int) -> str:
+def kaprekar(candidate: int) -> int:
     """
-    The function takes an integer input and returns the number of iterations it takes to reach
-    Kaprekar's constant.
+    The function takes a 4-digit integer with at least 2 different digits and returns the number of
+    iterations it takes to reach Kaprekar's constant.
 
-    :param candidate: an integer that represents the starting number for the Kaprekar's constant
-    algorithm. The function will iterate on this number until it reaches the Kaprekar's constant
+    :param candidate: The input number that we want to check if it can be transformed into Kaprekar's
+    constant through a series of operations
     :type candidate: int
-    :return: The function `kaprekar` returns a string that states how many iterations it takes for a
-    given number to reach Kaprekar's constant. The string includes the original number and the number of
-    iterations.
+    :return: the number of iterations it takes for a given 4-digit integer to reach Kaprekar's constant
+    (6174) using the Kaprekar routine. If the input is not a valid 4-digit integer with at least 2
+    different digits, an error message is returned.
     """
-
-    if candidate < 1000 or candidate > 9999 or not len(set(str(candidate))) >= 2:
-        return f"The number must be a 4-digit integer with at least 2 different digits"
-    elif candidate == KAPREKAR_CONSTANT:
-        return f"The number {candidate} is already Kaprekar's constant"
+    if candidate < 1000 or candidate > 9999 or len(set(str(candidate))) < 2:
+        return "The number must be a 4-digit integer with at least 2 different digits"
 
     number = candidate
     iterations = 0
 
-    while number != KAPREKAR_CONSTANT:
+    while number != 6174:
         number_str = str(number)
-        number = int(''.join(sorted(number_str, reverse=True))) - int(''.join(sorted(number_str)))
+        number = int("".join(sorted(number_str, reverse=True))) - int("".join(sorted(number_str))
+        )
         iterations += 1
-    return f"The number {candidate} reaches Kaprekar's constant after {iterations} iterations"
+
+    return iterations
